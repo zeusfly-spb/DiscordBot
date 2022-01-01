2df57d659a274281e3fe2c153e4c906262e1e3f9
Oleg Saifutdinov <zeusfly@mail.ru>
2022-01-02 01:59:39 +0300

    Some fixes

--- commands/members.js
+++ commands/members.js
@@ -1,7 +1,10 @@
 module.exports = {
   name: 'members',
   description: 'Отображает участников гильдии',
-  execute(message, args) {
+  execute(message) {
+    if (!message.guild.available) {
+      throw new Error('Невозможно получить доступ к экземпляру гильдии')
+    }
     members = message.guild.members.cache.map(member => {
       const string = `id: ${member.user.id}, username: ${member.user.username}`
       message.channel.send(string)
