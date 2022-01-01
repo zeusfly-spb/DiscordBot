8f880f3e54211a96a4eff11e36f2cb2b13646c07
Oleg Saifutdinov <zeusfly@mail.ru>
2022-01-02 01:42:05 +0300

    Added role ids

--- commands/roles.js
+++ commands/roles.js
@@ -2,11 +2,14 @@ module.exports = {
   name: 'roles',
   description: 'Отображает все роли гильдии или выбранного его участника ',
   async execute(message, args) {
+    if (!message.guild.available) {
+      throw new Error('Невозможно получить доступ к экземпляру гильдии')
+    }
     let roles
     const id = args.shift()
     if (id) {
       try {
-        const member = await message.guild.available && await message.guild.members.fetch(id)
+        const member = await message.guild.members.fetch(id)
         roles = member.roles
       } catch (e) {
         throw new Error('Аргументом должен быть валидный ID участника гильдии')
